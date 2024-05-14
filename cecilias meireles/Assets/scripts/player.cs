09aa76491bdacc6cffce07d81861ee9d25c2ca8e
LuisJahn <gustavo061108@gmail.com>
2024-05-14 16:02:55 -0300

    começação dos scirpts

---
+++ cecilias meireles/Assets/scripts/player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Rigidbody2D body;
+    public float speed = 5, jumpstrengh = 5;
+    float horizontal;
+    public bool groundCheck;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
