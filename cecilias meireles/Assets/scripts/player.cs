4a5f757950fa360341c10a7b75ec29bcec90f99c
LuisJahn <gustavo061108@gmail.com>
2024-05-15 10:31:00 -0300

    fiz certo os scripts

--- cecilias meireles/Assets/scripts/player.cs
+++ cecilias meireles/Assets/scripts/player.cs
@@ -9,6 +9,7 @@ public class player : MonoBehaviour
     public float speed = 5, jumpstrengh = 5;
     float horizontal;
     public bool groundCheck;
+    public Transform foot;
     void Start()
     {
         
@@ -17,6 +18,15 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // movimentação
+        horizontal = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2 (horizontal * speed, body.velocity.y);
+
+        //pulo
+        groundCheck = Physics2D.OverlapCircle(foot.position, 0.05f);
+        if (Input.GetButtonDown("Jump") && groundCheck)
+        {
+            body.AddForce  (new Vector2(0, jumpstrengh * 100));
+        }
     }
 }
