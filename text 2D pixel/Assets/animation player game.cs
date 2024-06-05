168ca5eee4535e6d8df55bd7bf11968743ba256f
FranSch2000 <chicoscheidt@gmail.com>
2024-06-05 08:52:47 -0300

    sprites
    Todas as animações do personagem (Idle)

---
+++ text 2D pixel/Assets/animation player game.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationplayergame : MonoBehaviour
+{
+    public int speed = 10;
+    private float horizontal;
+    private float vertical;
+    public int 
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("horizontal");
+        
+
+        
+    }
+}
