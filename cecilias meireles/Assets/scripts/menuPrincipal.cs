369de76ca1b2e04b8c9ab8317f7763e7a8494be3
LuisJahn <gustavo061108@gmail.com>
2024-08-08 14:41:44 -0300

    um monte de coisa

--- cecilias meireles/Assets/scripts/menuPrincipal.cs
+++ cecilias meireles/Assets/scripts/menuPrincipal.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class menuPrincipal : MonoBehaviour
 {
     
     public GameObject painelMenuInicial;
     public GameObject painelOpcoes;
+    public AudioMixer audiomixer;
+    
+
    
     public void jogar()
     {
@@ -31,5 +35,17 @@ public class menuPrincipal : MonoBehaviour
     public void sairJogo()
     {
         Application.Quit();
+    }
+    public void SetVolume(float volume)
+    {
+        audiomixer.SetFloat("Master", volume);
+    }
+    public void SetVolumeMusic(float volume)
+    {
+        audiomixer.SetFloat("Music", volume);
+    }
+    public void SetVolumeSFX(float volume)
+    {
+        audiomixer.SetFloat("SFX", volume);
     }
 }
