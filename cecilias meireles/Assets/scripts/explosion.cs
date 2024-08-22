7526b196ef836473e38aafc7ac96ea959baa9311
LuisJahn <gustavo061108@gmail.com>
2024-08-22 11:36:04 -0300

    dsa

--- cecilias meireles/Assets/scripts/explosion.cs
+++ cecilias meireles/Assets/scripts/explosion.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Rendering.Universal;
 
 public class explosion : MonoBehaviour
 {
@@ -10,18 +11,27 @@ public class explosion : MonoBehaviour
     public GameObject parede;
     public GameObject s;
     Animator animator;
+    public GameObject light;
+    public float time;
+    public bool luz;
     
     void Start()
     {
         booom = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
+        
     }
 
+    void t1me()
+    {
+        time += Time.deltaTime;
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -31,10 +41,30 @@ public class explosion : MonoBehaviour
             parede.SetActive(false);
             Destroy(gameObject, 2);
             animator.SetBool("kjk", true);
+           luz = true;
         }
         else 
         {
             animator.SetBool("kjk", false);
+            luz = false;
         }
+    }
+
+    void luzz()
+    {
+        if(luz == true) 
+        {
+            t1me();
+
+            if(time >=2 ) 
+            { 
+              light.SetActive(false );
+            }
+        }
+        else
+        {
+            light.SetActive(true);
+        }
+
     }
 }
