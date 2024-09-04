45f4d0a03c200b422ca439bc194569e9a3511afe
LuisJahn <gustavo061108@gmail.com>
2024-09-04 09:12:09 -0300

    organizei muito o script do player

--- cecilias meireles/Assets/scripts/player.cs
+++ cecilias meireles/Assets/scripts/player.cs
@@ -14,45 +14,53 @@ public class player : MonoBehaviour
     public GameObject fimDaBeta;
     public GameObject Texto;
 
-    [Header("resto(vou arrumar depois)")]
-    public ParticleSystem dust;
-     Rigidbody2D body;
-    [SerializeField] private float speed = 5, jumpstrengh = 5, bulletSpeed = 15;
-    float horizontal;
-    [SerializeField]private  bool groundCheck;
+    [Header("player")]
     public Transform foot;
-    public GameObject spawn;
-    public string faseParaCarregar;
-    public GameObject bullet;
-    int direction = 1;
-    public bool comLivro = false;
-    bool olhandoDireita;
-    public GameObject portal1;
-    public GameObject portal2;
-         Collider2D footCollision;
-    public static bool fase1 = false;
-    public bool noPortal = false;
-    public bool hub;
-    public float time;
-    public bool podePular = true;
-    public float doubleJump = 2;
-    public LayerMask layerMask;
-     private Animator animator;
-    private int maxJump = 2;
-    private int jumpsLeft;
-    public bool Doublejum;
-    
-   
-    
+     Rigidbody2D body_;
+    public ParticleSystem dust;
 
+    [Header("movimentação")]    
+    bool olhandoDireita_;
+    int direction_ = 1;
+    [SerializeField] private float speed_ = 5, jumpstrengh_ = 5, bulletSpeed_ = 15;
+    float horizontal_;
+
+    [Header("pulo e pulo duplo")]
+    [SerializeField]private  bool groundCheck_;
+    Collider2D footCollision;
+    private int maxJump_ = 2;
+    private int jumpsLeft;
+    public float doubleJump = 2;
+    public bool doublejum;
+    public bool podePular = true;
+
+
+    [Header("tiro")]
+    public GameObject bullet;
+    public bool comLivro = false;
+
+    [Header("tempo")]
+    public float time;
+
+    [Header("animação")]
+    private Animator animator_;
+
+
+
+
+
+    private void Awake()
+    {
+        olhandoDireita_ = true;
+        animator_ = GetComponent<Animator>();
+        body_ = GetComponent<Rigidbody2D>();
+        jumpsLeft = maxJump_;
+    }
 
     void Start()
     { 
-        olhandoDireita = true;
-        animator = GetComponent<Animator>();
-        body = GetComponent<Rigidbody2D>();
-        jumpsLeft = maxJump;
     }
+
     public void t1me()
     {
         time += Time.deltaTime;
@@ -60,53 +68,38 @@ public class player : MonoBehaviour
 
     void pulo()
     {
-        
-        
-        //OI professor, se tiver lendo os meus códigos, por favor, não chore de desgosto :3 
-        
     }
 
     void PuloDuplo()
     {
-
     }
     void Update()
     {
         
 
         // movimentação
-        horizontal = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontal * speed, body.velocity.y);
+        horizontal_ = Input.GetAxis("Horizontal");
+        body_.velocity = new Vector2(horizontal_ * speed_, body_.velocity.y);
        
-        
-        if(horizontal != 0) 
+        //animação de andar
+        if(horizontal_ != 0) 
         {
-            animator.SetBool("andando", true);
+            animator_.SetBool("andando", true);
         }
         else
         {
-            animator.SetBool("andando", false);
+            animator_.SetBool("andando", false);
 
         }
-
-        if(body.velocity.y != 0)
-        {
-            animator.SetBool("Is jumping", true);
-        }
-        else
-        {
-            animator.SetBool("Is jumping", false);
-
-        }
-
-        
 
         Flip();
+
+
         //pulo
 
-        // groundCheck = Physics2D.OverlapCircle(foot.position, 0.05f);
-        footCollision = Physics2D.OverlapCircle(foot.position, 0.05f, layerMask);
-        groundCheck = footCollision;
+        
+        footCollision = Physics2D.OverlapCircle(foot.position, 0.05f);
+        groundCheck_ = footCollision;
 
         if (footCollision != null)
         {
@@ -117,19 +110,19 @@ public class player : MonoBehaviour
                 //Mathf.Infinity;
                
               
-                body.AddForce(new Vector2(0, jumpstrengh * 120));
+                body_.AddForce(new Vector2(0, jumpstrengh_ * 120));
                 Destroy(footCollision.gameObject);
             }
             
         }
         //direção
-        if (horizontal < 0)
+        if (horizontal_ < 0)
         {
-            direction = -1;
+            direction_ = -1;
         }
-        else if (horizontal > 0)
+        else if (horizontal_ > 0)
         {
-            direction = 1;
+            direction_ = 1;
         }
 
 
@@ -137,34 +130,33 @@ public class player : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
 
-            if (!Doublejum && groundCheck)
+            if (!doublejum && groundCheck_)
             {
-                body.AddForce(new Vector2(0, jumpstrengh * 100));
+                body_.AddForce(new Vector2(0, jumpstrengh_ * 100));
                 CreateDust();
 
             }
-            else if(Doublejum && jumpsLeft >0)
+            else if(doublejum && jumpsLeft >0)
             {
-                body.AddForce(new Vector2(0, jumpstrengh * 100));
+                body_.AddForce(new Vector2(0, jumpstrengh_ * 100));
                 CreateDust();
                 jumpsLeft -= 1;
 
             }
-            
-
+       
         }
         
 
-        if (body.velocity.y < 0 && groundCheck == true)
+        if (body_.velocity.y < 0 && groundCheck_ == true)
         {
-            jumpsLeft = maxJump;
+            jumpsLeft = maxJump_;
         }
 
         //atirar
         if (Input.GetButtonDown("Fire1") && comLivro == true) 
         {
             GameObject temp = Instantiate(bullet, transform.position, transform.rotation);
-            temp.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed * direction, 0);
+            temp.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed_ * direction_, 0);
         }
     }
    
@@ -173,43 +165,20 @@ public class player : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Pulo"))
         {
-            Doublejum = true;
+            doublejum = true;
             Destroy(collision.gameObject);
         }
 
-        if (collision.gameObject.CompareTag("caiuvolta"))
+        /*if (collision.gameObject.CompareTag("caiuvolta"))
         {
-            body.MovePosition(spawn.transform.position);
-        }
-
+            body_.MovePosition(spawn.transform.position);
+        } talvez eu use para checkpont, criar uma variavel public gameObject spawn;
+        */
         if(collision.gameObject == fimDaBeta)
         {
             Texto.SetActive(true);
         }
-        
-
-        //sistema de respawn
-        if (collision.gameObject.CompareTag("fase1"))
-        {
-            faseParaCarregar = "fase1";
-        }
-        if (collision.gameObject.CompareTag("fase2"))
-        {
-            faseParaCarregar = "fase2";
-        }
-        if (collision.gameObject.CompareTag("fase3"))
-        {
-            faseParaCarregar = "fase3";
-        }
-        if (collision.gameObject.CompareTag("fase4"))
-        {
-            faseParaCarregar = "fase4";
-        }
-        if (collision.gameObject.CompareTag("faseFinal"))
-        {
-            faseParaCarregar = "faseFinal";
-        }
-
+       
         //pegar livro power up
         if(collision.gameObject.CompareTag("livroPowerUp"))
         {
@@ -220,74 +189,20 @@ public class player : MonoBehaviour
         //morrer pro canhão
         if (collision.gameObject.CompareTag("cannonBall"))
         {
-            SceneManager.LoadScene(faseParaCarregar);
+            SceneManager.LoadScene("fase1");
         }
 
         //caiu no void
         if (collision.gameObject.CompareTag("queda"))
         {
-            SceneManager.LoadScene(faseParaCarregar);
+            SceneManager.LoadScene("fase1");
         }
 
-        if(collision.gameObject == portal1 && collision.gameObject == portal2)
-        {
-            podePular = false;
-        }
-       else
-        {
-            podePular = true;
-        }
         
 
     }
    
-    //entrar nas portas
-    private  void OnTriggerStay2D(Collider2D collider)
-    {
-
-        if (collider.gameObject.CompareTag("hub"))
-        {
-            fase1 = false;
-        }
-
-        if (collider.gameObject.CompareTag("porta"))
-        {
-            t1me();
-
-            if (time >= 0.5f)
-            {
-                SceneManager.LoadScene("fase1");
-                fase1 = true;
-                time = 0;
-            }
-        }
-        //entrar nos portais da fase 1
-        if (collider.gameObject.CompareTag("portal1"))
-        {
-            t1me();
-            if (time >= 1)
-            {
-                transform.position = new Vector2(-414.7f, 4f);
-                time = 0;
-            }
-        }
-        
-        if (collider.gameObject.CompareTag("portal2") )
-        {
-            t1me();
-            if (time >= 1)
-            {
-                transform.position = new Vector2(-420.61f, 4f);
-                time = 0;
-            }
-        }
-
-        // entrar no final 1
-        if(collider.gameObject.CompareTag("FINAL1"))
-        {
-            body.velocity = new Vector2(0, 2);
-        }
-    }
+    
 
     // morrer pros inimigos
     private void OnCollisionEnter2D(Collision2D collision)
@@ -315,18 +230,16 @@ public class player : MonoBehaviour
     // virar o sprite
     void Flip()
     {
-        if (horizontal > 0 && !olhandoDireita || horizontal < 0 && olhandoDireita)
+        if (horizontal_ > 0 && !olhandoDireita_ || horizontal_ < 0 && olhandoDireita_)
         {
-            olhandoDireita = !olhandoDireita;
+            olhandoDireita_ = !olhandoDireita_;
             Vector2 localscale = transform.localScale;
             localscale.x *= -1;
             transform.localScale = localscale;
         }
     }
-
-
     void CreateDust()
     {
-            dust.Play();
+       dust.Play();
     }
 }
