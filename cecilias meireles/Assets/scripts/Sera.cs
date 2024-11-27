35c83c41d29ba1dd38894a8e3b576b3056623689
LuisJahn <167345643+LuisNaiate@users.noreply.github.com>
2024-11-27 19:54:14 -0300

    teste

---
+++ cecilias meireles/Assets/scripts/Sera.cs
@@ -0,0 +1,30 @@
+Using UnityEngine;
+
+public class Sera : MonoBehavior 
+{
+  int num1;
+  int num2;
+  int num3;
+
+
+  Void Start()
+  {
+   int valor total;
+   num1 = 1;
+   num2 = 89;
+   num3 = 4;
+
+   print(valorTotal);
+   valorTotal = num1 * num 2 / num3;
+
+   if(valorTotal > 0) 
+   {
+    print("oloko");
+   }
+    else 
+   {
+     print("bah meu");
+   }
+  }
+
+}
